93648fed7db67add75c4722559cd572df43e7cc4
Naveen Kumar Rai <naveen.1910rai@gmail.com>
2017-11-22 15:40:24 -0500

    Check-in #2

--- classify/mnist_classify.py
+++ classify/mnist_classify.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 # To run tensor board
@@ -17,7 +17,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 
-# In[9]:
+# In[2]:
 
 
 sample_image_d = mnist.train.next_batch(1)
@@ -32,7 +32,7 @@ plt.imshow(sample_image, cmap='Greys')
 
 # Softmax regression model with a single linear layer.
 
-# In[14]:
+# In[3]:
 
 
 import tensorflow as tf
@@ -63,7 +63,7 @@ print(accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 # Build a Multilayer Convolutional Network
 
-# In[1]:
+# In[ ]:
 
 
 import tensorflow as tf
@@ -129,14 +129,14 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 # Run
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
-  for i in range(20000):
+  for i in range(200):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
       train_accuracy = accuracy.eval(feed_dict={
           x: batch[0], y_: batch[1], keep_prob: 1.0})
       print('step %d, training accuracy %g' % (i, train_accuracy))
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-
-print('test accuracy %g' % accuracy.eval(feed_dict={
-      x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    
+  print('test accuracy %g' % accuracy.eval(feed_dict={
+    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
