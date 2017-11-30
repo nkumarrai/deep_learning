fa50160ebb458a866cb6f61b22417b9764d49398
Roy Shilkrot <roys@cs.stonybrook.edu>
2017-11-30 02:12:35 -0500

    Check-in #14

--- gan/rico_dataset_gan.py
+++ gan/rico_dataset_gan.py
@@ -18,7 +18,7 @@ How to generate input for this encoder?
 2. These layout images are the inputs to this gan.
 '''
 
-BATCH_SIZE = 500
+BATCH_SIZE = 50
 epoch_num = 5     # Number of epochs to train the network
 lr = 0.001        # Learning rate
 
@@ -26,7 +26,7 @@ def discriminator(images, reuse_variables=None):
     with tf.variable_scope(tf.get_variable_scope(), reuse=reuse_variables) as scope:
         # First convolutional and pool layers
         # This finds 32 different 5 x 5 pixel features
-        d_w1 = tf.get_variable('d_w1', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
+        d_w1 = tf.get_variable('d_w1', [5, 5, 3, 32], initializer=tf.truncated_normal_initializer(stddev=0.02))
         d_b1 = tf.get_variable('d_b1', [32], initializer=tf.constant_initializer(0))
         d1 = tf.nn.conv2d(input=images, filter=d_w1, strides=[1, 1, 1, 1], padding='SAME')
         d1 = d1 + d_b1
@@ -68,21 +68,21 @@ z_dim = 100
 [-1, 1920, 1080, 25] x [1, 1, 25, 1] (strides = [1,2,2,1]) -> [-1, 960, 540, 3]
 '''
 def generator(z, batch_size, z_dim):
-    g_w1 = tf.get_variable('g_w1', [z_dim, 6220800], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
-    g_b1 = tf.get_variable('g_b1', [6220800], initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_w1 = tf.get_variable('g_w1', [z_dim, 1555200], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_b1 = tf.get_variable('g_b1', [1555200], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g1 = tf.matmul(z, g_w1) + g_b1
-    g1 = tf.reshape(g1, [-1, 1920, 1080, 3])
+    g1 = tf.reshape(g1, [-1, 960, 540, 3])
     g1 = tf.contrib.layers.batch_norm(g1, epsilon=1e-5, scope='bn1')
     g1 = tf.nn.relu(g1)
 
     # Generate 50 features
-    g_w2 = tf.get_variable('g_w2', [3, 3, 1, z_dim/2], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
+    g_w2 = tf.get_variable('g_w2', [3, 3, 3, z_dim/2], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
     g_b2 = tf.get_variable('g_b2', [z_dim/2], initializer=tf.truncated_normal_initializer(stddev=0.02))
     g2 = tf.nn.conv2d(g1, g_w2, strides=[1, 2, 2, 1], padding='SAME')
     g2 = g2 + g_b2
     g2 = tf.contrib.layers.batch_norm(g2, epsilon=1e-5, scope='bn2')
     g2 = tf.nn.relu(g2)
-    g2 = tf.image.resize_images(g2, [1920, 1080])
+    g2 = tf.image.resize_images(g2, [960, 540])
 
     # Generate 25 features
     g_w3 = tf.get_variable('g_w3', [3, 3, z_dim/2, z_dim/4], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
@@ -91,7 +91,7 @@ def generator(z, batch_size, z_dim):
     g3 = g3 + g_b3
     g3 = tf.contrib.layers.batch_norm(g3, epsilon=1e-5, scope='bn3')
     g3 = tf.nn.relu(g3)
-    g3 = tf.image.resize_images(g3, [1920, 1080])
+    g3 = tf.image.resize_images(g3, [960, 540])
 
     # Final convolution with one output channel
     g_w4 = tf.get_variable('g_w4', [1, 1, z_dim/4, 3], dtype=tf.float32, initializer=tf.truncated_normal_initializer(stddev=0.02))
@@ -100,7 +100,7 @@ def generator(z, batch_size, z_dim):
     g4 = g4 + g_b4
     g4 = tf.sigmoid(g4)
     
-    # Dimensions of g4: batch_size x 960 x 540 x 3
+    # Dimensions of g4: BATCH_SIZE x 960 x 540 x 3
     return g4
 
 def shuffle_train_data(train_filepaths):
@@ -117,7 +117,7 @@ def data_iterator(num_train):
         yield files_batch
 
 def load_files(files_batch_val):
-    w, h = 960, 540
+    w, h = 960/2, 540/2
     input_data = np.zeros((BATCH_SIZE,w,h,3))
     for i in range(len(files_batch_val)):
         img = cv2.imread(files_batch_val[i])
@@ -168,18 +168,19 @@ train_filepaths, test_filepaths = generate_train_test_data(file, dataset_path)
 
 print "BATCH_SIZE %d and epochs %d"%(BATCH_SIZE,epoch_num)
 iter_ = data_iterator(len(train_filepaths))
+sess = tf.Session()
 
 #######################################
 # GAN network code starts here
 #######################################
-
+z_dimensions = 100
 z_placeholder = tf.placeholder(tf.float32, [None, z_dimensions], name='z_placeholder') 
 # z_placeholder is for feeding input noise to the generator
 
-x_placeholder = tf.placeholder(tf.float32, shape = [None,960,540,3], name='x_placeholder') 
+x_placeholder = tf.placeholder(tf.float32, shape = [None,960/2,540/2,3], name='x_placeholder') 
 # x_placeholder is for feeding input images to the discriminator
 
-Gz = generator(z_placeholder, batch_size, z_dimensions) 
+Gz = generator(z_placeholder, BATCH_SIZE, z_dimensions) 
 # Gz holds the generated images
 
 Dx = discriminator(x_placeholder) 
@@ -214,29 +215,28 @@ tf.summary.scalar('Generator_loss', g_loss)
 tf.summary.scalar('Discriminator_loss_real', d_loss_real)
 tf.summary.scalar('Discriminator_loss_fake', d_loss_fake)
 
-images_for_tensorboard = generator(z_placeholder, batch_size, z_dimensions)
+images_for_tensorboard = generator(z_placeholder, BATCH_SIZE, z_dimensions)
 values_for_tensorboard = discriminator(x_placeholder) 
 tf.summary.image('Generated_images', images_for_tensorboard, 5)
 merged = tf.summary.merge_all()
-logdir = "tensorboard/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
+logdir = "tensorboard/" + datetime.now().strftime("%Y%m%d-%H%M%S") + "/"
 writer = tf.summary.FileWriter(logdir, sess.graph)
 
 #############################################
 # Initialize the network and start training.
 #############################################
 
-sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 # Add ops to save and restore all the variables.
 saver = tf.train.Saver()
 
 # Pre-train discriminator
 for i in range(300):
-    z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
+    z_batch = np.random.normal(0, 1, size=[BATCH_SIZE, z_dimensions])
     try:
         files_batch_val = iter_.next()
         batch_images = load_files(files_batch_val)
-        print "train batch_images.shape", batch_images.shape
+        print "train batch_images.shape", batch_images.shape, " iteration in pre-train discriminator", i
     except StopIteration:
         print "Something is wrong, this loop had a small run, couldn't exhaust an epoch!"
         break
@@ -262,25 +262,25 @@ for ep in range(epoch_num):  # epochs loop
             print "Current epoch is done, data was processed in BATCH_SIZE %d" % BATCH_SIZE
             break
 
-        z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
+        z_batch = np.random.normal(0, 1, size=[BATCH_SIZE, z_dimensions])
         # Train discriminator on both real and fake images
         __, __, dLossReal, dLossFake = sess.run([d_trainer_real, d_trainer_fake, d_loss_real, d_loss_fake],
                                            {x_placeholder: real_image_batch, z_placeholder: z_batch})
 
         # Train generator
-        z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
+        z_batch = np.random.normal(0, 1, size=[BATCH_SIZE, z_dimensions])
         __ = sess.run(g_trainer, feed_dict={z_placeholder: z_batch})
 
         if i % 10 == 0:
             # Update TensorBoard with summary statistics
-            z_batch = np.random.normal(0, 1, size=[batch_size, z_dimensions])
+            z_batch = np.random.normal(0, 1, size=[BATCH_SIZE, z_dimensions])
             summary = sess.run(merged, {z_placeholder: z_batch, x_placeholder: real_image_batch})
             writer.add_summary(summary, i)
 
         if i % 100 == 0:
             print("dLossReal:", dLossReal, "dLossFake:", dLossFake)
             # Every 100 iterations, show a generated image
-            print("Iteration:", i, "at", datetime.datetime.now())
+            print("Iteration:", i, "at", str(datetime.now()))
             z_batch = np.random.normal(0, 1, size=[1, z_dimensions])
             generated_images = generator(z_placeholder, 1, z_dimensions)
             images = sess.run(generated_images, {z_placeholder: z_batch})
